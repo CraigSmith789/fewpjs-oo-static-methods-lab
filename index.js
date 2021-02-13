3b7a9f12637dadd78a9bdece974d4140c32bc36f
CraigSmith789 <craig789@gmail.com>
2021-02-13 13:54:00 -0500

    commit tree b0eb09aeb7b768e86b38ac1cd973c3bbc21dcd97

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
 class Formatter {
   //add static methods here
+
+
+  static capitalize(string) {
+    // capitalizes first letter
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    // removes any non alpha-numeric characters except dash and single quotes (apostrophes)
+    return string.replace(/[^A-Za-z0-9-' ]+/g, '');
+  }
+  static titleize(str) {
+    let i, j, lowers;
+    str = str.replace(/([^\W_]+[^\s-]*) */g, function (txt) {
+      return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    });
+
+    lowers = ['A', 'An', 'The', 'And', 'But', 'For', 'At',
+      'By', 'For', 'From', 'Of'];
+    for (i = 0, j = lowers.length; i < j; i++)
+      str = str.replace(new RegExp('\\s' + lowers[i] + '\\s', 'g'),
+        function (txt) {
+          return txt.toLowerCase();
+        });
+    return str;
+  }
 }
